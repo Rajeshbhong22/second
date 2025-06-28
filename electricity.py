50d4e73adfe9a8662d5fd4cd650406a902e8eca3
Rajeshbhong22 <bhongrajesh2@gmail.com>
2025-06-28 16:36:27 +0530

    created chamges

--- electricity.py
+++ electricity.py
@@ -1,14 +1,19 @@
+print("this is june month after june this is not considred")
 print("welcome to electricity bill payment website, save electricity, save future")
 print("Deadline of the electricity bill payment is 30 june 2025")
 print("pay the payment before dead line, after deadline payment fine will be 200rs")
 print("hope you are payment done before dead line, Thank You!")
 name=input("enter your name:")
-total_units=int(input("enter the total year units:"))
-date=30
+date=int(input("enter your date payment:"))
 if name=="rajesh":
+    total_units=int(input("enter your total units:" ))
     bill=total_units*15
     print("Total bill of the payment:",bill,"rs")
-    if date>30:
-        fine_bill=total_units*
-
-
+    if 1<=date<=29:
+        bill_payment=total_units*15
+        print("your total bill is:", bill_payment)
+    else:
+        fine_bill=(total_units*15)+200
+        print("your pay after dead line so, 200rs fine added in you regular bill payment:",fine_bill)
+else:
+    print("invalid name")
--- function.py
+++ function.py
@@ -1,4 +1,79 @@
-file=open("practice.txt","a")
-data=file.write("hi\n" "raj\n")
+# heads=int(input("enter the total heads:"))
+# legs=int(input("enter the total legs:"))
+# goats=(legs-(2*heads))/2
+# chickens=(4*heads-(legs))/2
+# print("total number of goats are:",goats)
+# print("total number of chickens are:", chickens)
 
-print(data)
+#creating tuple
+a=(1,2,3,4)
+print(a)
+print(type(a))
+
+#empty tuple
+tup=()
+print(tup)
+print(type(tup))
+#single element
+b=(5)
+print(b)
+print(type(b))
+b=(5,)
+print(b)
+print(type(b))
+#single element do not contain tuple
+
+#a tuple containing with mixed data types
+b=(1,23,"swati","nishant",True)
+print(b)
+print(type(b))
+
+#nested tuple
+a=(1,2.3,"mouse",(10,20,30))
+print(a)
+print(type(a))
+a=(1,2,3,"rajesh",[10,20,30])
+print(a)
+print(type(a))
+
+#tuple constructor
+empty=tuple()
+print(empty)
+print(type(empty))
+
+#converting list into a tuple
+list=[10,20,30,40,50,60,70,80]
+print(tuple(list),type(tuple(list)))
+
+#converting string into tuple
+
+String_example="hello"
+print(type(String_example))
+tuple1=tuple(String_example)
+print((tuple1))
+
+#conveting dictionary into tuple
+dict_example={"a":1,"b":2,"c":6}
+print(dict_example)
+print(type(dict_example))
+tuple1=tuple(dict_example)
+print(tuple1)
+
+#converting a set into a tuple
+set_example={1,2,3,4,5,6}
+print(set_example)
+print(type(set_example))
+tup=tuple(set_example)
+print(tup)
+print()
+print()
+
+#reversing the tuple
+t=(1,2,3,4,5,6,7,8)
+print(t[::])
+print(t[3:])
+print(t[:4])
+print(t[-1:2:-1])
+print()
+t=(1,2,3,4)
+del t
